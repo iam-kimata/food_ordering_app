0e1dd6f7dbeee8b7f05c9ef7c190363f51d02f10
iam-kimata <kimataaloyce444@gmail.com>
2026-06-28 12:22:01 +0300

    Initial Commit

---
+++ lib/auth/login.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:interview_demo_app/auth/register.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+
+                const SizedBox(height: 40),
+
+                Container(
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.symmetric(horizontal: 25),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        controller: usernameController,
+                        hintText: "Email",
+                        obscureText: false,
+                      ),
+
+                      const SizedBox(height: 15),
+
+                      CustomTextField(
+                        controller: passwordController,
+                        hintText: "Password",
+                        obscureText: true,
+                      ),
+
+                      const SizedBox(height: 25),
+
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.blue,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "LOGIN",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => RegisterPage()),
+                          );
+                        },
+                        child: Text(
+                          "Don't have an account? Register",
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      autocorrect: false,
+      enableSuggestions: false,
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.white54),
+        filled: true,
+        fillColor: Colors.grey,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/auth/register.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  RegisterPage({super.key});
+
+  final firstNameController = TextEditingController();
+  final lastNameController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+
+                const SizedBox(height: 40),
+
+                Container(
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.symmetric(horizontal: 25),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Column(
+                    children: [
+                      InputFile(
+                        label: "First Name",
+                        controller: firstNameController,
+                      ),
+                      InputFile(
+                        label: "Last Name",
+                        controller: lastNameController,
+                      ),
+                      InputFile(
+                        label: "Phone Number",
+                        controller: phoneNumberController,
+                      ),
+                      InputFile(
+                        label: "Email",
+                        controller: emailController,
+                      ),
+                      InputFile(
+                        label: "Password",
+                        controller: passwordController,
+                        obscureText: true,
+                      ),
+                      InputFile(
+                        label: "Confirm Password",
+                        controller: confirmPasswordController,
+                        obscureText: true,
+                      ),
+
+                      const SizedBox(height: 25),
+
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.blue,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "REGISTER",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: Text(
+                          "Already have an account? Login",
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InputFile extends StatelessWidget {
+  final String label;
+  final TextEditingController controller;
+  final bool obscureText;
+
+  const InputFile({
+    super.key,
+    required this.label,
+    required this.controller,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final decoration = InputDecoration(
+      filled: true,
+      fillColor: Colors.grey,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide.none,
+      ),
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+        const SizedBox(height: 10),
+
+        Text(
+          label,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        const SizedBox(height: 5),
+
+        TextField(
+          controller: controller,
+          obscureText: obscureText,
+          autocorrect: false,
+          enableSuggestions: false,
+          style: TextStyle(color: Colors.white),
+          decoration: decoration,
+        ),
+      ],
+    );
+  }
+}
