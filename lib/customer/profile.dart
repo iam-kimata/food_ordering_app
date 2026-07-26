6125fd1d91ba8295baa0735063e33f0372a8cc45
iam-kimata <kimataaloyce444@gmail.com>
2026-07-26 11:25:38 +0300

    Implementation of Bottom Navigation Bar

--- lib/customer/profile.dart
+++ lib/customer/profile.dart
@@ -11,6 +11,7 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       body: Padding(
         padding: EdgeInsets.all(22),
         child: Column(
